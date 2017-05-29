1bee42ed9f3bd85b88783a8f5900c78d873454b2
Nikhil Agarwal <TheNik@Nikhils-MacBook-Air.local>
2017-05-29 00:15:57 -0700

    Finished call.s and description

--- cs313/hw1/call.s
+++ cs313/hw1/call.s
@@ -1,20 +1,13 @@
 .pos 0x100
 	xorq %rdx, %rdx
-	irmovq $b, %rbx
-	call *0x1(%rbx), %rax
+	irmovq 0x1000, %rbx
+	call *0x100(%rbx), %rax
+	# call *0x1000(%rbx), %rax
 	iaddq 0x10, %rdx
 	halt
-
-.pos 0x1000
-	pushq %rax
-	iaddq 0x2, %rdx
-	ret	
 
 .pos 0x1100
 	pushq %rax
 	iaddq 0x3, %rdx
 	ret
-
-.pos 0x2000
-b:		.quad 0x00001000
 
