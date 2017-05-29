0d85f1ad20a1675aa0a6ab997c0108b61ef55064
Victorree <victorree33@gmail.com>
2017-05-28 23:53:19 -0700

    Add files via upload

--- cs313/hw1/call.s
+++ cs313/hw1/call.s
@@ -1,2 +1,20 @@
-main:
-	
+.pos 0x100
+	xorq %rdx, %rdx
+	irmovq $b, %rbx
+	call *0x1(%rbx), %rax
+	iaddq 0x10, %rdx
+	halt
+
+.pos 0x1000
+	pushq %rax
+	iaddq 0x2, %rdx
+	ret	
+
+.pos 0x1100
+	pushq %rax
+	iaddq 0x3, %rdx
+	ret
+
+.pos 0x2000
+b:		.quad 0x00001000
+
